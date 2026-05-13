6f1a5f0d3c6ad7630f294e94f27bb20c9d9dbaae
MaraGreuter <126458316+MaraGreuter@users.noreply.github.com>
2026-05-13 09:14:05 +0200

    updated map controller

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
 
 class MapsController extends Controller
 {
-    public function index()
+    public function index(request $request)
     {
-//        $location =
-//        $lat = $request->input('latitude');
+        //$location =
+        $lat = $request->input('latitude');
 
         return view('home');
     }
